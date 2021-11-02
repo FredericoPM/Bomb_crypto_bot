06f0496f5c922ca61777b028debb37b3d95b8152
Frederico Prado Marques <fredericopradomarques@gmail.com>
2021-11-02 09:51:56 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -103,7 +103,7 @@ class Bot:
                 except: 
                     time.sleep(self._small_time)
                     if(self.is_image_present("./images/metamask_sign_tab.png")):     
-                        self.await_for_image("./images/metamask_sign_tab.png", self._medium_time)
+                        self.await_and_click("./images/metamask_sign_tab.png", self._medium_time)
                     else:
                         print("Metamask tab not founded")
                     time.sleep(self._small_time)
